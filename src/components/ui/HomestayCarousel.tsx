475642179eaac496db102e182812a5cc6bce5243
Edward Akapo <edwardakapo@outlook.com>
2024-08-06 13:14:24 -0400

    finished locaiton section and updated gitignore to not track some public pictures

--- src/components/ui/HomestayCarousel.tsx
+++ src/components/ui/HomestayCarousel.tsx
@@ -1,60 +1,27 @@
 import React from 'react'
 import type { EmblaOptionsType } from 'embla-carousel'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'
-import {
-  PrevButton,
-  NextButton,
-  usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
 import useEmblaCarousel from 'embla-carousel-react'
 import '@/styles/homestayCarousel.css'
+import Autoplay from 'embla-carousel-autoplay'
 // import '@/styles/base.module.css'
 type PropType = {
-  slides: number[]
+  images : string[]
   options?: EmblaOptionsType
 }
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props
-  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const { images, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay({delay : 3000})])
 
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
-
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
 
   return (
-    <section className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">{index + 1}</div>
+    <section className="embla_2">
+      <div className="embla__viewport_2" ref={emblaRef}>
+        <div className="embla__container_2">
+          {images.map((x) => (
+            <div className="embla__slide_2" key={x}>
+                <img className="embla__slide__image" src={x}/>
             </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="embla__controls">
-        <div className="embla__buttons">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
-              )}
-            />
           ))}
         </div>
       </div>
