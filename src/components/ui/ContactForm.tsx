3f06d6e30d25933ce39da13b8ddb13402bd3c87a
Edward Akapo <edwardakapo@outlook.com>
2024-08-13 14:17:46 -0400

    email form updated and live, site is ready for live

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -7,14 +7,83 @@ import {
     SelectValue,
   } from "@/components/ui/select"
 import {Toaster, toast} from 'sonner'
+import emailjs from "@emailjs/browser"
 
-
+type TemplateParams = {
+    name: string;
+    phone: string;
+    EDD: string;
+    typeOfDelivery: string;
+    accommodation: boolean;
+    postnatalCare: boolean;
+    hasDate: boolean;
+    arrivalDate: string;
+    hasLocation: boolean;
+    arrivalLocation: string;
+    hasExtraInfo: boolean;
+    extraInformation: string;
+};
 
 export default function MyForm() {
-    const [name, setName] = useState("");
+
+    const [formData, setFormData] = useState({
+        name: '',
+        phone: '',
+        expectedDeliveryDate: '',
+        typeOfDelivery: '',
+        accommodation: false,
+        postnatalCare: false,
+        arrivalDate: '',
+        arrivalLocation: '',
+        extraInformation: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { name, value, type } = e.target as HTMLInputElement; // Type assertion added
+        const checked = type === 'checkbox' ? (e.target as HTMLInputElement).checked : undefined; // Handle checkbox separately
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: type === 'checkbox' ? checked : value
+        }));
+    }
     function submitHandler() {
+        if (!formData.name || !formData.phone || !formData.expectedDeliveryDate || !formData.typeOfDelivery) {
+            toast.error('Please fill out first 4 required fields');
+            return;
+        }
+
+        const serviceId = import.meta.env.PUBLIC_EMAILJS_SERVICE_ID;
+        const templateId = import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID;
+        const publicKey = import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY;
         // e.preventdefault()
-        const promise = () => new Promise((resolve) => setTimeout(() => resolve({ name: 'Sonner' }), 1500));
+        console.log(formData)
+
+        const templateParams: TemplateParams = {
+            name : formData.name,
+            phone : formData.phone,
+            EDD : formData.expectedDeliveryDate,
+            typeOfDelivery : formData.typeOfDelivery,
+            accommodation : formData.accommodation,
+            postnatalCare : formData.postnatalCare,
+            hasDate : formData.arrivalDate !== '',
+            arrivalDate : formData.arrivalDate,
+            hasLocation : formData.arrivalLocation !== '',
+            arrivalLocation : formData.arrivalLocation,
+            hasExtraInfo : formData.extraInformation !=='',
+            extraInformation : formData.extraInformation,
+        }
+        
+        const promise = new Promise((resolve, reject) => {
+            emailjs.send(serviceId, templateId, templateParams as unknown as Record<string, unknown>, publicKey)
+            .then((response) => {
+                console.log("Email sent successfully", response);
+                resolve(response); // Resolve the promise on success
+            })
+            .catch((error) => {
+                console.log("Error sending email", error);
+                reject(error); // Reject the promise on error
+            });
+        });
 
         toast.promise(promise, {
           loading: 'Loading...',
@@ -24,15 +93,21 @@ export default function MyForm() {
           error: 'Email failed to send, use other contact methods to reach out',
         });
     }
+
     return (
+        <div>
+        <Toaster richColors position='bottom-right'/>
         <form className='flex flex-wrap justify-between w-[550px] mobile-md:w-[250px] gap-y-8 mt-3 '>
-            <Toaster richColors position='bottom-right'/>
             <div className='flex flex-col w-[250px]'>
                 <label className="w-fit mb-3" htmlFor='name'> Name </label> 
                 <input className='border border-black p-2 rounded-sm'
                     id='name'
                     type="text"
-                    value={name}
+                    name='name'
+                    value={formData.name}
+                    onChange={handleChange}
+                    autoComplete='name'
+                    required
                 />
             </div>
             <div className='flex flex-col w-[250px]'>
@@ -40,18 +115,30 @@ export default function MyForm() {
                 <input className='border border-black p-2 rounded-sm'
                     id='phone'
                     type="tel"
+                    name='phone'
+                    autoComplete='tel-national'
+                    required
+                    value={formData.phone}
+                    onChange={handleChange}
+
                 />
             </div>
             <div className='flex flex-col w-[250px]'>
-                <label className="w-fit mb-3" htmlFor='name'> Expected Delivery Date </label> 
+                <label className="w-fit mb-3" htmlFor='EDD'> Expected Delivery Date </label> 
                 <input className='border border-black p-2 rounded-sm'
-                    id='name'
+                    id='EDD'
                     type="date"
+                    name='expectedDeliveryDate'
+                    required
+                    value={formData.expectedDeliveryDate}
+                    onChange={handleChange}
+
                 />
             </div>
             <div className='flex flex-col w-[250px]'>
                 <label className="w-fit mb-3"> Type Of Delivery </label> 
-                <Select>
+                {/* <Select onValueChange={handleChange} > */}
+                <Select required onValueChange={(value) => setFormData(prevState => ({ ...prevState, typeOfDelivery: value }))}>
                     <SelectTrigger className="w-[250px] h-[44px] border-black">
                         <SelectValue placeholder="Select one...." />
                     </SelectTrigger>
@@ -64,36 +151,58 @@ export default function MyForm() {
             </div>
             <div className='flex w-[250px]  gap-x-2 items-center'>
                 <input
-                    id='name'
+                    id='Accomodation'
                     type="checkbox"
+                    name='accommodation'
+                    checked={formData.accommodation}
+                    onChange={handleChange}
                 />
-                <label htmlFor='name'> Accommodation </label> 
+                <label htmlFor='Accomodation'> Accommodation </label> 
             </div>
             <div className='flex w-[250px] gap-x-2 items-center'>
                 <input
-                    id='name'
+                    id='pnc'
+                    name='postnatalCare'
                     type="checkbox"
+                    checked={formData.postnatalCare}
+                    onChange={handleChange}
                 />
-                <label htmlFor='name'> Postnatal (AB) Care </label> 
+                <label htmlFor='pnc'> Postnatal (AB) Care </label> 
             </div>
-            <div className='flex flex-col w-[250px]'>
-                <label className="w-fit mb-3" htmlFor='arrival-date'> Date of Arrival </label> 
-                <input className='border border-black p-2 rounded-sm'
-                    id='arrival-date'
-                    type="date"
-                />
-            </div>
-            <div className='flex flex-col w-[250px]'>
-                <label className="w-fit mb-3" htmlFor='arrival-location'> Location for Pickup </label> 
-                <input className='border border-black p-2 rounded-sm'
-                    id='arrival-location'
-                    type="text"
-                    value={name}
-                />
+            <div className='flex flex-wrap gap-y-8 justify-between w-full'>
+
+                <div className='flex flex-col w-[250px]'>
+                    <label className="w-fit mb-3" htmlFor='arrival-date'> Date of Arrival </label> 
+                    <input className='border border-black p-2 rounded-sm'
+                        id='arrival-date'
+                        type="date"
+                        name='arrivalDate'
+                        value={formData.arrivalDate}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='flex flex-col w-[250px]'>
+                    <label className="w-fit mb-3" htmlFor='arrival-location'> Location for Pickup </label> 
+                    <input className='border border-black p-2 rounded-sm'
+                        id='arrival-location'
+                        type="text"
+                        name='arrivalLocation'
+                        value={formData.arrivalLocation}
+                        onChange={handleChange}
+                    />
+                </div>
             </div>
             <div className='flex flex-col w-full'>
                 <label className="w-fit mb-3" htmlFor='typeOfDelivery'> Extra Information </label> 
-                <textarea className="border resize-none border-black p-2 rounded-sm" rows={4} cols={50} id='typeOfDelivery' placeholder='Any extra information you want to tell us....'>
+                <textarea className="border resize-none border-black p-2 rounded-sm" 
+                rows={4} 
+                cols={50} 
+                id='typeOfDelivery' 
+                placeholder='Any extra information you want to tell us....'
+                name='extraInformation'
+                value={formData.extraInformation}
+                onChange={handleChange}
+                >
                     
                 </textarea>
             </div>
@@ -102,5 +211,7 @@ export default function MyForm() {
             </button>
 
         </form>
+        </div>
+
     )
 }
